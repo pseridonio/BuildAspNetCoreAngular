14922b82fc3c44dd2edf0de89890eecfaf4419ed
Peterson Seridonio <petersonseridonio@gera.com.br>
2020-08-15 20:01:18 -0300

    Creating the first APIs

---
+++ BuildAppDotNetCoreAngular/BuildAppDotNetCoreAngular.API/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildAppDotNetCoreAngular.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildAppDotNetCoreAngular.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DatabaseContext _dbContext;
+
+        public ValuesController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            List<Value> values = _dbContext.Values.ToList();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            Value value = _dbContext.Values.Find(id);
+
+            if (value == null)
+                return NotFound();
+
+            return Ok(value);
+        }
+    }
+}
---
+++ BuildAppDotNetCoreAngular/BuildAppDotNetCoreAngular.Database/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BuildAppDotNetCoreAngular.API.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
